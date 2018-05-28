02a82470b2c5c1a092c708333ea2389c2f1d8096
krijovnichek <tyukaevruslan@gmail.com>
2018-05-28 20:01:58 +0400

    add gitignore

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require( 'express' );
+var	bodyParser = require('body-parser');
+var	app = express();
+var	server = require('http').createServer(app);
+var url = require('url');
+var fs = require('fs'); 
+var http = require('http');
+
+server.listen(process.env.PORT || 3000, function(){
+	console.log('Start listening 3000...');
+} );
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/public/index.html');
+});
